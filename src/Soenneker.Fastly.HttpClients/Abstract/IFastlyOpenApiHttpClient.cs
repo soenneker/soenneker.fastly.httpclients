ee19ab01fd70bbbe519ade752692cf2c4ec079d5
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:11 -0500

    xml commenting

--- src/Soenneker.Fastly.HttpClients/Abstract/IFastlyOpenApiHttpClient.cs
+++ src/Soenneker.Fastly.HttpClients/Abstract/IFastlyOpenApiHttpClient.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Fastly.HttpClients.Abstract;
 /// </summary>
 public interface IFastlyOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
